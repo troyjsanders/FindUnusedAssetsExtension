6082ba1d00cd996cc6f6055876ec09deb575d5c4
durmazoguzhan <durmazoguzhan@yahoo.com>
2023-11-18 01:50:41 +0300

    first release

--- README.md
+++ README.md
@@ -1,65 +1,34 @@
-# FUA README
+# FUA
 
-This is the README for your extension "FUA". After writing up a brief description, we recommend including the following sections.
+FUA is a "Find and Remove Unused Assets" VS Code extension.
+You can check and remove/delete your project's unused assets/images.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+### Supported Extensions
 
-For example if there is an image subfolder under your extension project workspace:
+#### Supported Asset File Extensions
 
-\!\[feature X\]\(images/feature-x.png\)
+- .jpg
+- .jpeg
+- .png
+- .svg
+- .gif
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+#### Supported Component File Extensions
 
-## Requirements
+- .html
+- .css
+- .js
+- .jsx
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+## Using
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+- Open a workspace/folder in VS Code.
+- Sure that your workspace has "src" folder (because FUA working on src file).
+- Press 'F1' key and enter `Find Unused Assets` command.
+- After a short period of time You will see the `UnusedAssets.txt` file. You can check this file and delete the lines you don't want.
+- Lastly, Press 'F1' key and enter `Remove Unused Assets` command.
+- After a short period of time your unused assets will be removed.
 
 **Enjoy!**
--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 const vscode = require("vscode");
+const { open, readFile, access, constants, unlink } = require("fs/promises");
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -19,10 +20,35 @@ async function activate(context) {
 
 async function findUnusedAssets() {
   if (await isFindable()) {
-    vscode.window.showInformationMessage("works!");
-    let workspacePath = vscode.workspace.workspaceFolders[0].uri.path;
-    let workspaceFsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-    // Continue with your logic here
+    const assets = await getAssets();
+    vscode.window.showInformationMessage("Total Asset Count: " + assets.length);
+    const components = await getComponents();
+    vscode.window.showInformationMessage("Total Component Count: " + components.length);
+    let unusedAssetPaths = [];
+
+    for (let i = 0; i < assets.length; i++) {
+      let fileName = getFileName(assets[i]);
+      if (!(await isFileUsing(fileName, components))) {
+        unusedAssetPaths.push(assets[i].fsPath);
+      }
+    }
+    await createUnusedAssetsFile(unusedAssetPaths);
+    vscode.window.showInformationMessage(
+      "Done! Unused Asset Files Count: " + unusedAssetPaths.length.toString()
+    );
+  }
+}
+
+async function removeUnusedAssets() {
+  if (await isRemovable()) {
+    const file = await open(getUnusedAssetsFilePath());
+    const unusedAssets = (await readFile(file)).toString().split("\n");
+    for (let i = 0; i < unusedAssets.length; i++) {
+      unlink(unusedAssets[i]);
+    }
+    await file.close();
+    unlink(getUnusedAssetsFilePath());
+    vscode.window.showInformationMessage("Done! All unused assets were deleted.");
   }
 }
 
@@ -30,6 +56,76 @@ async function isFindable() {
   if (isWorkspaceOpen()) {
     return await hasSrcFolder();
   } else return false;
+}
+
+async function isRemovable() {
+  if (isWorkspaceOpen() && (await hasSrcFolder()) && (await hasUnusedAssetsFile())) return true;
+  else return false;
+}
+
+async function hasUnusedAssetsFile() {
+  try {
+    await access(getUnusedAssetsFilePath(), constants.R_OK);
+    return true;
+  } catch (error) {
+    vscode.window.showErrorMessage(
+      "The UnusedAssets.txt file not found. Please run 'Find Unused Assets' command first."
+    );
+    return false;
+  }
+}
+
+async function createUnusedAssetsFile(assetPaths) {
+  try {
+    const file = await open(getUnusedAssetsFilePath(), "w");
+    await file.writeFile("");
+    if (assetPaths.length > 0) {
+      await file.appendFile(assetPaths[0]);
+      for (let i = 1; i < assetPaths.length; i++) {
+        await file.appendFile("\n");
+        await file.appendFile(assetPaths[i]);
+      }
+    }
+    await file.close();
+  } catch (error) {
+    vscode.window.showErrorMessage(
+      "Something went wrong when creating UnusedAssets.txt file: \n" + error.message
+    );
+  }
+}
+
+async function isFileUsing(fileName, components) {
+  let isUsing = false;
+  try {
+    for (let i = 0; i < components.length; i++) {
+      const file = await open(components[i].fsPath, "r");
+      if ((await readFile(file)).toString().includes(fileName)) {
+        isUsing = true;
+        await file.close();
+        break;
+      }
+      await file.close();
+    }
+  } catch (error) {
+    vscode.window.showErrorMessage("Got an error trying to check the file using: " + error.message);
+  }
+  return isUsing;
+}
+
+function getUnusedAssetsFilePath() {
+  return vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, "UnusedAssets.txt").fsPath;
+}
+
+function getFileName(uri) {
+  return uri.path.split("/").pop();
+}
+
+async function getAssets() {
+  return await vscode.workspace.findFiles("src/**/*.{png,jpg,jpeg,gif,svg}");
+}
+
+async function getComponents() {
+  return await vscode.workspace.findFiles("src/**/*.{html,css,js,jsx}");
 }
 
 async function hasSrcFolder() {
@@ -46,8 +142,6 @@ function isWorkspaceOpen() {
     return false;
   } else return true;
 }
-
-async function removeUnusedAssets() {}
 
 function deactivate() {}
 
